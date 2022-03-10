d47ca34f31710bad1137fc7430c188b6ee0ceb8c
eoinkane <eoinkane23@gmail.com>
2022-03-10 23:36:45 +0000

    Added some work-in-progress basic functionality
    Main sections include:
    * Displaying a sudoku board
    * storing a sudoku board as a matrix
    * and some housekeeping repository files

---
+++ src/utils/custom_types.py
@@ -0,0 +1,5 @@
+from typing import List
+
+Board = List[List[int]]
+
+Column_References = List[str]
---
+++ src/utils/screen.py
@@ -0,0 +1,20 @@
+import platform
+import os
+from typing import List
+from .custom_types import Board, Column_References
+
+
+def clear_screen():
+    if platform.system() == "Linux" or platform.system() == "Darwin":
+        os.system("clear")
+    elif platform.system() == "Windows":
+        os.system("cls")
+    print()
+
+def print_sudoku_board(board: Board, column_references: Column_References):
+	clear_screen()
+	print("      " + "   ".join(column_references) )
+	print("    _____________________________________")
+	for row_index, row in enumerate(board):
+		print(f"{row_index + 1} - | {' | '.join(str(item) for item in row).replace('0', ' ')} |")
+	print("    ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯")

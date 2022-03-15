d7654589c78af3fe47e09513ff8b3d3ec9269c31
eoin.kane <eoin.kane@postcodelottery.co.uk>
2022-03-15 21:19:23 +0000

    get sub grid helper

--- src/utils/board.py
+++ src/utils/board.py
@@ -5,3 +5,46 @@ from .custom_types import Board, Column_References
 
 def get_column_references(board: Board) -> Column_References:
     return list(string.ascii_lowercase[0:len(board[0])].upper())
+
+
+def get_sub_grid_indexes(grid_id: int):
+    if (grid_id < 1 or grid_id > 9):
+        raise Exception("invalid input to get_sub_grid_indexes")
+
+    column_indexes = None
+    if (grid_id <= 3):
+        column_indexes = [
+            x - 1 for x in [x + (3 * (grid_id - 1)) for x in range(1, 4)]
+        ]
+        row_indexes = [x for x in range(3)]
+    elif (grid_id % 3 == 1):
+        row_indexes = [x for x in range(grid_id - 1, grid_id + 2)]
+    elif (grid_id % 3 == 2):
+        row_indexes = [x for x in range(grid_id - 2, grid_id + 1)]
+    else:
+        row_indexes = [x for x in range(grid_id - 3, grid_id)]
+
+    if (column_indexes is None):
+        column_indexes = [
+            x - 1 for x in [
+                x + (3 * ((grid_id - 1) % 3)) for x in range(1, 4)
+            ]]
+
+    return {
+        "row_indexes": row_indexes,
+        "column_indexes": column_indexes
+    }
+
+
+def get_sub_grid(sub_grid_indexes, board: Board, grid_id: int) -> Board:
+    sub_grid_indexes = get_sub_grid_indexes(grid_id)
+    return [
+        item[
+            sub_grid_indexes["column_indexes"][0]:
+            sub_grid_indexes["column_indexes"][-1] + 1
+        ]
+        for item in board[
+            sub_grid_indexes["row_indexes"][0]:
+            sub_grid_indexes["row_indexes"][-1] + 1
+        ]
+    ]
